dc9b69e64bf599b1175396faa42263effd59b883
DJ Kaswa <74205036+DJKaswa@users.noreply.github.com>
2021-05-07 11:15:01 -0700

    Update -tags.tf
    added back tags

--- -tags.tf
+++ -tags.tf
@@ -1,13 +1,13 @@
-# locals {
-#   common_tags = {
-#     Environment = var.env_name
-#     SourceRepo  = var.source_repo
-#     Developer   = "StratusGrid"
-#     Provisioner = "Terraform"
-#     Terraform   = "true"
-#     ModuleSourceRepo = "github.com/StratusGrid/terraform-aws-acm-certificate-creation"
-#   }
-# }
+locals {
+  common_tags = {
+    Environment = var.env_name
+    SourceRepo  = var.source_repo
+    Developer   = "StratusGrid"
+    Provisioner = "Terraform"
+    Terraform   = "true"
+    ModuleSourceRepo = "github.com/StratusGrid/terraform-aws-acm-certificate-creation"
+  }
+}
 
 //locals {
 //  local_tags = merge(var.input_tags, {
